f18af44cc7e75b813dd657d45b5fca0ce4822125
NURUDDIN <mdnuruddin043@gmail.com>
2020-09-25 20:52:29 +0600

    added new links

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const tiktok=require('tiktok-scraper')
-
+const request = require('request');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,19 +10,72 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 
-app.post('/metadata', async (req, res,next)=>{
-	try{
-        const url=req.body.url
-        const videoMeta = await tiktok.getVideoMeta(url);
-        res.send(videoMeta);
+const getMetaData=async (req,res,next)=>{
+    try{
+        const videoMetadata = await tiktok.getVideoMeta(req.params[0]);
+        const newUrl=`https://www.tiktok.com/@${videoMetadata.authorMeta.name}/video/${videoMetadata.id}`
+        req.newUrl=newUrl
+        next()
                 
 	}catch(e){
 		next(e)
 	}
+}
+const getId=(req, res, next)=>{
+    try{
+        const fecth=`https://tiktok.codespikex.com/api/v1/fetch?url=${req.newUrl}`
+
+        request({url:fecth,headers: {
+            'TOKEN': '715f22af-c70b-4850-9598-d27aa625f835'
+        },json : true 
+        },(error,res,body)=>{ 
+            if(error){
+                throw new Error('not connected')
+            }else if(body.error){
+                throw new Error('no result found')
+            }else{
+                req.video_id=body.video_id
+            }
+            next();
+        })
+        
+    }catch(e){
+        next(e)
+    }
+	
+	
+}
+const getVideo=(req, res,next)=>{
+	const fecth=`https://tiktok.codespikex.com/api/v1/fetch-videos/${req.video_id}`
+
+    request({url:fecth,headers: {
+        'TOKEN': '715f22af-c70b-4850-9598-d27aa625f835'
+    },json : true },(error,res,body)=>{ 
+        if(error){
+            throw new Error('not connected')
+        }else if(body.error){
+			throw new Error('no result found')
+        }else{
+            next();
+		}
+ 
+	})
+	
+}
+const finalCallBack=function (req, res) {
+	try{
+				
+        const url=`https://tiktok.codespikex.com/download?id=${req.video_id}&type=video&nwm=true`;
+        res.redirect(url);
+		
+	}catch(e){
+		next(e)
+	}
   
-});
+};
 
 
+app.get('/*',[getMetaData,getId,getVideo,finalCallBack])
 
 /////error handling route
 app.use((req,res,next)=>{
